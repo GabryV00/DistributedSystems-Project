61e3e323f46cf35d99e97925af0bb207dce8effb
Gabriele Voltan <voltan.gabriele.official@gmail.com>
2024-03-25 17:40:16 +0100

    adding all the functions

--- src/init/init.py
+++ src/init/init.py
@@ -1,9 +1,12 @@
 import os
 import json
+import random
 import argparse
 
 def create_json(numNodes):
-    
+    """
+    Function that creates all the json files
+    """
     if not("config_files" in os.listdir(".")):
         os.mkdir("./config_files")
     
@@ -15,9 +18,73 @@ def create_json(numNodes):
 
         with open(f'./config_files/node_{i}.json', "w") as f:
             f.write(json.dumps(dictionary))
+    
+    print("All json files created!")
         
+def checkEdge(nodeA, nodeB):
+    """
+    Function that checks if already exists an edge from A to B
+    """
+    with open(f'./config_files/node_{nodeA}.json', "r") as f:
+        a_edges = json.load(f)["edges"]
+    
+    for e in a_edges:
+        if e[0] == nodeB:
+            return True
+    
+    return False
 
+def writeEdge(nodeA, nodeB, band):
+    """
+    Function that add a new edge in the json files
+    """
+    with open(f'./config_files/node_{nodeA}.json', "r") as f:
+        data = json.load(f)
+    
+    data["edges"].append([nodeB, band])
 
+    with open(f'./config_files/node_{nodeA}.json', "w") as f:
+        json.dump(data, f)
+    
+    with open(f'./config_files/node_{nodeB}.json', "r") as f:
+        data = json.load(f)
+    
+    data["edges"].append([nodeA, band])
+
+    with open(f'./config_files/node_{nodeB}.json', "w") as f:
+        json.dump(data, f)
+
+def checkOneForAll(numNodes):
+    """
+    Function that check that all the peer are connected with at least one peer
+    """
+    for i in range(1, numNodes+1):
+        with open(f'./config_files/node_{i}.json', "r") as f:
+            a_edges = json.load(f)["edges"]
+        if a_edges == []:
+            b = i
+            while b != i:
+                b = random.randint(1, numNodes)
+            band = random.randint(5, 100)
+            writeEdge(i, b, band)
+
+def create_edges(numEdges, numNodes):
+    """
+    Function that create the edges
+    """
+    i = 0
+    while i != numEdges:
+        a,b = 0, 0
+        while a == b:
+            a = random.randint(1, numNodes)
+            b = random.randint(1, numNodes)
+        if not checkEdge(a,b):
+            band = random.randint(5, 100)
+            writeEdge(a, b, band)
+            i += 1 
+    
+    checkOneForAll(numNodes)
+    print("All edges created!")
 
 if __name__ == "__main__":
 
@@ -32,6 +99,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     create_json(args.numNodes)
+
+    maxNumEdges = int(args.numNodes * (args.numNodes - 1) / 2)
+    numEdges = int(maxNumEdges / 100 * args.edges)
+
+    create_edges(numEdges=numEdges, numNodes=args.numNodes)
+
     
     
 
