4399eec57fffe12010155eb50ddbd1e82ff2fc60
Gabriele Voltan <voltan.gabriele.official@gmail.com>
2024-03-26 11:51:40 +0100

    adding details to the graph

--- src/webgui/main.py
+++ src/webgui/main.py
@@ -27,22 +27,25 @@ def send_request(id_peer, band):
 
 
 def generate_graph(PATH):
+    """
+    Function that generates the graph of the newtork reading the json files
+    """
     if not("graph" in os.listdir(".")):
         os.mkdir("./graph")
     
-    g = graphviz.Graph("P2P Streaming Network", strict=True)
+    g = graphviz.Graph("P2P Streaming Network", strict=True, format='png')
+    g.attr('node', shape='circle')
     nodes = os.listdir(PATH)
     for n in range(len(nodes)):
-        #print(nodes[n][5: len(nodes[n])-5])
         id = nodes[n][5: len(nodes[n])-5]
-        g.node(f'{id}', f'Peer {id}')
+        g.node(f'{id}', f'Peer {id}', color='red')
     
     for f in nodes:
         id_a = f[5: len(f)-5]
         with open(f'{PATH}/{f}', "r") as fl:
             edges = json.load(fl)["edges"]
         for n in edges:
-            g.edge(f'{id_a}', f'{n[0]}', label=f'{n[1]}')
+            g.edge(f'{id_a}', f'{n[0]}', label=f'{n[1]}', color='blue')
 
 
     g.render('./graph/graph.gv').replace('\\', '/')
