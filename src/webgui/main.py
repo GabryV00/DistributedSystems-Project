86d3f08b5a32243d8eee63ea0df55c96f273134e
Gabriele Voltan <voltan.gabriele.official@gmail.com>
2024-04-09 17:24:37 +0200

    show opening conn

--- src/webgui/main.py
+++ src/webgui/main.py
@@ -14,6 +14,14 @@ TCP_ADDR = "127.0.0.1"
 
 LOG_FILE_NAME = '../../p2p_app/logs/log.txt'
 
+OPEN_CONN = []
+
+def parse_list_to_string(input_list):
+    result = ""
+    for sublist in input_list:
+        result += f"NodeA: {sublist[0]}, NodeB: {sublist[1]}, Band: {sublist[2]}\n"
+    return result
+
 def interpret_response(data):
     """
     Function that takes bytes in inputs and return the interpretation of the json
@@ -153,6 +161,11 @@ def remove_node():
     # print(f'NodeID to remove: {node_id}')
     
     ris, msg = send_kill(node_id)
+    if [node_id, _, _] in OPEN_CONN:
+        OPEN_CONN.remove([node_id, _, _])
+    if [_, node_id, _] in OPEN_CONN:
+        OPEN_CONN.remove([_, node_id, _])
+
     generate_graph("./../init/config_files")
     with open(LOG_FILE_NAME, 'r') as file:
         content = file.read()
@@ -164,7 +177,7 @@ def remove_node():
 @app.route("/peer", methods=['GET', 'POST'])
 def peer():
     if request.method == 'GET':
-        return render_template('peer.html')
+        return render_template('peer.html',  oc=OPEN_CONN)
     
     id_peerA = request.form.get('idpa')
     id_peerB = request.form.get('idpb')
@@ -176,18 +189,25 @@ def peer():
     #send data to Erlang node via TCP
 
     ris, msg = open_conn(id_peerA, id_peerB, band)
-    return render_template('peer.html', status=ris, message=msg)
+
+    if 'ok' in ris:
+        OPEN_CONN.append([id_peerA, id_peerB, band])
+    return render_template('peer.html', status=ris, message=msg, oc=parse_list_to_string(OPEN_CONN))
 
 @app.route("/close_conn", methods=['GET', 'POST'])
 def close_conn():
     if request.method == 'GET':
-        return render_template('peer.html')
+        return render_template('peer.html',  oc=OPEN_CONN)
     
     id_peerA = request.form.get('idpa')
     id_peerB = request.form.get('idpb')
 
     ris, msg = close_con(id_peerA, id_peerB)
-    return render_template('peer.html', status=ris, message=msg)
+    if [id_peerA, id_peerB, _] in OPEN_CONN:
+        OPEN_CONN.remove([id_peerA, id_peerb, _])
+    if [id_peerB, id_peerA, _] in OPEN_CONN:
+        OPEN_CONN.remove([id_peerB, id_peerA, _])
+    return render_template('peer.html', status=ris, message=msg,  oc=parse_list_to_string(OPEN_CONN))
 
 #----------------------LOG---------------------------------
 @app.route("/log/")
