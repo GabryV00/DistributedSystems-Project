6946f277bd1a21bf663aa77d09c32125a718e5a5
gianzav <gianlucagianluca18@gmail.com>
2024-04-11 12:25:52 +0200

    startup scripts

---
+++ launch_p2p.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd src/init
+if [ $# -eq 0 ];
+then
+  python3 init.py
+  echo "Network created with 10 nodes and 80% of density (standard parameters)!"
+else
+  python3 init.py -n $1 -e $2
+  echo "Network created with $1 nodes and $2% of density!"
+fi
+
+
+cd ..
+cd ..
+cd p2p_app
+rebar3 escriptize
+_build/default/bin/p2p -i "../src/init/config_files/"
+
+echo "Erlang app launched!"
---
+++ launch_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd src/webgui
+
+python3 main.py
+
+echo "WebGUI launched!"
